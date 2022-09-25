a0345060b0f71cfbc67802e6210b09c349c4ed2d
Unicorn <97339986+Unicornel@users.noreply.github.com>
2022-09-25 13:06:43 +0200

    Made another Script for smol objects uwu
    Sodass ich nicht alle einzeln anklicken muss lel

---
+++ kuschelige-spiel-marmelade/Assets/SCRIPTS/ClicktobigforsmollerObjects.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ClicktobigforsmollerObjects : MonoBehaviour
+{
+    // Refer to the Canvas Group put into one Item
+    [SerializeField] 
+    private Canvas canvas;
+    private RectTransform rectTransform;
+    private CanvasGroup canvasGroup;
+    private Vector3 bigScale, smallScale;
+    private bool isBig;
+    public float newScale = 1f;
+    private Vector3 scaleVector;
+
+    // So that Canvas and Slot actually works with one another
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+        void Start()
+    {
+        scaleVector.Set(newScale, newScale, 0.3f);
+        this.transform.localScale = scaleVector;
+        
+        bigScale = new Vector3(1f, 1f, 1f);
+        smallScale = new Vector3(0.3f, 0.3f, 0.3f);
+        isBig = true;
+    }
+
+    // Begin to Drag the Item, so you can click on it and have it attached to ur mouse
+   public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnBeginDrag");
+        canvasGroup.blocksRaycasts = false;
+    }
+   
+   // So you can Drag the Item everywhere
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnDrag");
+        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+    }
+
+    // Snatching to the Canvas made easy
+   public void OnEndDrag(PointerEventData eventDate)
+   {
+        Debug.Log("OnEndDrag");
+        canvasGroup.blocksRaycasts = true;
+   }
+   
+    // Press mouse Button
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        transform.localScale = isBig ? smallScale : bigScale;
+        isBig = !isBig;
+        Debug.Log("OnPointerDown");
+    }
+}
